d9997739eac83081add63fde452763d069ea8bfe
Antonio Robson de Paula <robson.depaula@gmail.com>
2020-09-22 16:36:31 -0300

    Adding request camera permission dialog. Closes #8

---
+++ UnityAndroidCamera/Assets/Scripts/CameraPermissionTest.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+#if PLATFORM_ANDROID
+using UnityEngine.Android;
+#endif
+
+public class CameraPermissionTest : MonoBehaviour
+{
+    GameObject dialog = null;
+    
+    void Start ()
+    {
+        #if PLATFORM_ANDROID
+        if (!Permission.HasUserAuthorizedPermission(Permission.Camera))
+        {
+            Permission.RequestUserPermission(Permission.Camera);
+            dialog = new GameObject();
+        }
+        #endif
+    }
+
+    void OnGUI ()
+    {
+        #if PLATFORM_ANDROID
+        if (!Permission.HasUserAuthorizedPermission(Permission.Camera))
+        {
+            // The user denied permission to use the camera.
+            // Display a message explaining why you need it with Yes/No buttons.
+            // If the user says yes then present the request again
+            // Display a dialog here.
+            dialog.AddComponent<PermissionsRationaleDialog>();
+            return;
+        }
+        else if (dialog != null)
+        {
+            Destroy(dialog);
+        }
+        #endif
+    }
+}
---
+++ UnityAndroidCamera/Assets/Scripts/PermissionsRationaleDialog.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+#if PLATFORM_ANDROID
+using UnityEngine.Android;
+#endif
+
+public class PermissionsRationaleDialog : MonoBehaviour
+{
+    const int kDialogWidth = 300;
+    const int kDialogHeight = 100;
+    private bool windowOpen = true;
+
+    void DoMyWindow(int windowID)
+    {
+        GUI.Label(new Rect(10, 20, kDialogWidth - 20, kDialogHeight - 50), "Please let me use the camera. It is mandatory!");
+        GUI.Button(new Rect(10, kDialogHeight - 30, 100, 20), "No");
+        if (GUI.Button(new Rect(kDialogWidth - 110, kDialogHeight - 30, 100, 20), "Yes"))
+        {
+            #if PLATFORM_ANDROID
+            Permission.RequestUserPermission(Permission.Camera);
+            #endif
+            windowOpen = false;
+        }
+    }
+
+    void OnGUI ()
+    {
+        if (windowOpen)
+        {
+            Rect rect = new Rect((Screen.width / 2) - (kDialogWidth / 2), (Screen.height / 2) - (kDialogHeight / 2), kDialogWidth, kDialogHeight);
+            GUI.ModalWindow(0, rect, DoMyWindow, "Permissions Request Dialog");
+        }
+    }
+}
